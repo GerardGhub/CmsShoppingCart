9bfc8c8f49cca811218f99f332395bd9abe7d424
voja <voja@gmail.com>
2019-11-05 18:57:19 +0100

    09

--- CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CmsShoppingCart.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,7 @@ namespace CmsShoppingCart.Infrastructure
             :base(options)
         {
         }
+
+        public DbSet<Page> Pages { get; set; }
     }
 }
