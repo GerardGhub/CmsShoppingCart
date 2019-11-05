994836cea1f94abd787f85f044edbbf6c3553494
voja <voja@gmail.com>
2019-11-05 09:44:27 +0100

    db

---
+++ CmsShoppingCart/Infrastructure/CmsShoppingCartContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Infrastructure
+{
+    public class CmsShoppingCartContext : DbContext
+    {
+        public CmsShoppingCartContext(DbContextOptions<CmsShoppingCartContext> options)
+            :base(options)
+        {
+        }
+    }
+}
