9d14edef84347d45724b56a868dfbbf8803a95b9
voja <voja@gmail.com>
2019-11-06 09:20:27 +0100

    19

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -44,6 +44,29 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         // GET /admin/pages/create
         public IActionResult Create() => View();
 
+        // POST /admin/pages/create
+        [HttpPost]
+        public async Task<IActionResult> Create(Page page)
+        {
+            if (ModelState.IsValid)
+            {
+                page.Slug = page.Title.ToLower().Replace(" ", "-");
+                page.Sorting = 100;
 
+                var slug = await context.Pages.FirstOrDefaultAsync(x => x.Slug == page.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The title already exists.");
+                    return View(page);
+                }
+
+                context.Add(page);
+                await context.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(page);
+        }
     }
 }
