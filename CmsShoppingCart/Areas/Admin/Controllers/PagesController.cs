45b5cb0ee67d8aed101c22a866d5d09fcf276f08
voja <voja@gmail.com>
2019-11-06 06:53:35 +0100

    13

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -2,16 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CmsShoppingCart.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PagesController : Controller
     {
-        public string Index()
+        private readonly CmsShoppingCartContext context;
+
+        public PagesController(CmsShoppingCartContext context)
         {
-            return "test";
+            this.context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IQueryable<Page> pages = from p in context.Pages orderby p.Sorting select p;
+
+            List<Page> pagesList = await pages.ToListAsync();
+
+            return View(pagesList);
         }
     }
 }
