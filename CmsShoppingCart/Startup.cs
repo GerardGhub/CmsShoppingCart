8344b3736a6e23a0d371b2f04a27192620165183
voja <voja@gmail.com>
2019-11-06 06:43:57 +0100

    12

--- CmsShoppingCart/Startup.cs
+++ CmsShoppingCart/Startup.cs
@@ -53,6 +53,11 @@ namespace CmsShoppingCart
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
