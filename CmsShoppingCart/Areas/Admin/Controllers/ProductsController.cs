8207a18c2905d01da0ec659727a5348e1361a883
voja <voja@gmail.com>
2019-11-07 09:59:28 +0100

    35

---
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext context;
+
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        // GET /admin/products
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Products.OrderByDescending(x => x.Id).Include(x => x.Category).ToListAsync());
+        }
+    }
+}
