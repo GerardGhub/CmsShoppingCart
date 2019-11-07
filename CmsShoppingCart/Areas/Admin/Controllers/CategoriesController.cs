87716332f959be290b4017f4b99bd1b3ed5332db
voja <voja@gmail.com>
2019-11-07 08:18:28 +0100

    28

---
+++ CmsShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+
+        private readonly CmsShoppingCartContext context;
+
+        public CategoriesController(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
