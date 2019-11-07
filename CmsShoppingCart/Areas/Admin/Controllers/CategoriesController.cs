1b405eb352b4d6d62c4ffbe5973c472f19fb5481
voja <voja@gmail.com>
2019-11-07 08:41:50 +0100

    30

--- CmsShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/CategoriesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,9 +20,41 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
             this.context = context;
         }
 
+        // GET /admin/categories
         public async Task<IActionResult> Index()
         {
             return View(await context.Categories.OrderBy(x => x.Sorting).ToListAsync());
+        }
+
+        // GET /admin/categories/create
+        public IActionResult Create() => View();
+
+        // POST /admin/categories/create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                category.Slug = category.Name.ToLower().Replace(" ", "-");
+                category.Sorting = 100;
+
+                var slug = await context.Categories.FirstOrDefaultAsync(x => x.Slug == category.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The category already exists.");
+                    return View(category);
+                }
+
+                context.Add(category);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The category has been added!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
         }
     }
 }
