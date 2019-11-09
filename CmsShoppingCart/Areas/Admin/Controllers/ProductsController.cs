11b77ca6f215ba04a421e04abab903dcea2d2146
voja <voja@gmail.com>
2019-11-09 06:35:22 +0100

    47

--- CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -114,5 +114,55 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
 
             return View(product);
         }
+
+        // POST /admin/products/edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Product product)
+        {
+            ViewBag.CategoryId = new SelectList(context.Categories.OrderBy(x => x.Sorting), "Id", "Name", product.CategoryId);
+
+            if (ModelState.IsValid)
+            {
+                product.Slug = product.Name.ToLower().Replace(" ", "-");
+
+                var slug = await context.Products.Where(x => x.Id != id).FirstOrDefaultAsync(x => x.Slug == product.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "The product already exists.");
+                    return View(product);
+                }
+
+                if (product.ImageUpload != null)
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+
+                    if (!string.Equals(product.Image, "noimage.png"))
+                    {
+                        string oldImagePath = Path.Combine(uploadsDir, product.Image);
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName;
+                    string filePath = Path.Combine(uploadsDir, imageName);
+                    FileStream fs = new FileStream(filePath, FileMode.Create);
+                    await product.ImageUpload.CopyToAsync(fs);
+                    fs.Close();
+                    product.Image = imageName;
+                }
+
+                context.Update(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been edited!";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
     }
 }
