7fd46713182582852be3f5f3a596872cd58eb32a
voja <voja@gmail.com>
2019-11-09 06:41:03 +0100

    48

--- CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/ProductsController.cs
@@ -164,5 +164,35 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
 
             return View(product);
         }
+
+        // GET /admin/products/delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            Product product = await context.Products.FindAsync(id);
+
+            if (product == null)
+            {
+                TempData["Error"] = "The product does not exist!";
+            }
+            else
+            {
+                if (!string.Equals(product.Image, "noimage.png"))
+                {
+                    string uploadsDir = Path.Combine(webHostEnvironment.WebRootPath, "media/products");
+                    string oldImagePath = Path.Combine(uploadsDir, product.Image);
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                context.Products.Remove(product);
+                await context.SaveChangesAsync();
+
+                TempData["Success"] = "The product has been deleted!";
+            }
+
+            return RedirectToAction("Index");
+        }
     }
 }
