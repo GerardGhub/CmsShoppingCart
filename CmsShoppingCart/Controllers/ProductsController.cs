eb241131af31215a6300b74f986388059af63a25
voja <voja@gmail.com>
2019-11-09 09:06:51 +0100

    52

---
+++ CmsShoppingCart/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly CmsShoppingCartContext context;
+
+        public ProductsController(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        // GET /products
+        public async Task<IActionResult> Index(int p = 1)
+        {
+            int pageSize = 6;
+            var products = context.Products.OrderByDescending(x => x.Id)
+                                            .Skip((p - 1) * pageSize)
+                                            .Take(pageSize);
+
+            ViewBag.PageNumber = p;
+            ViewBag.PageRange = pageSize;
+            ViewBag.TotalPages = (int)Math.Ceiling((decimal)context.Products.Count() / pageSize);
+
+            return View(await products.ToListAsync());
+        }
+    }
+}
