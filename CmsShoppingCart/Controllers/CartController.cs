9aba80302bd7c8fa441356567cd825afbd5efe87
voja <voja@gmail.com>
2019-11-09 10:57:22 +0100

    56

---
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly CmsShoppingCartContext context;
+
+        public CartController(CmsShoppingCartContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
