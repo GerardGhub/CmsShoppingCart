0b92203ecec85c9802cd366ffe9d50b65459cee8
voja <voja@gmail.com>
2019-11-10 06:37:27 +0100

    58

--- CmsShoppingCart/Controllers/CartController.cs
+++ CmsShoppingCart/Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CmsShoppingCart.Infrastructure;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsShoppingCart.Controllers
@@ -16,9 +17,18 @@ namespace CmsShoppingCart.Controllers
             this.context = context;
         }
 
+        // GET /cart
         public IActionResult Index()
         {
-            return View();
+            List<CartItem> cart = HttpContext.Session.GetJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+
+            CartViewModel cartVM = new CartViewModel
+            {
+                CartItems = cart,
+                GrandTotal = cart.Sum(x => x.Price * x.Quantity)
+            };
+
+            return View(cartVM);
         }
     }
 }
