42885d045e280e427582dd14a3c7bf032a0627ae
voja <voja@gmail.com>
2019-11-10 06:43:33 +0100

    59

--- CmsShoppingCart/Startup.cs
+++ CmsShoppingCart/Startup.cs
@@ -25,6 +25,13 @@ namespace CmsShoppingCart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+            services.AddSession(options =>
+            {
+                //options.IdleTimeout = TimeSpan.FromSeconds(2)
+                //options.IdleTimeout = TimeSpan.FromDays(2)
+            });
+
             services.AddControllersWithViews();
 
             services.AddDbContext<CmsShoppingCartContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CmsShoppingCartContext")));
@@ -47,6 +54,8 @@ namespace CmsShoppingCart
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseSession();
 
             app.UseAuthorization();
 
