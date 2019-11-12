c6c89e9b246fe23c5304a078ef30f22c43f73f8e
voja <voja@gmail.com>
2019-11-12 16:06:32 +0100

    73

---
+++ CmsShoppingCart/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmsShoppingCart.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        User() { }
+
+        public User(AppUser appUser)
+        {
+            UserName = appUser.UserName;
+            Email = appUser.Email;
+            Password = appUser.PasswordHash;
+        }
+    }
+}
