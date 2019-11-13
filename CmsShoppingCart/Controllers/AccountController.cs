241c264ba0cb4b3788560aefbfa0ee2ba9b09c8c
voja <voja@gmail.com>
2019-11-13 08:23:42 +0100

    76

--- CmsShoppingCart/Controllers/AccountController.cs
+++ CmsShoppingCart/Controllers/AccountController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CmsShoppingCart.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CmsShoppingCart.Controllers
@@ -10,8 +12,46 @@ namespace CmsShoppingCart.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private readonly UserManager<AppUser> userManager;
+
+        public AccountController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
         // GET /account/register
         [AllowAnonymous]
         public IActionResult Register() => View();
+        
+        // POST /account/register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser
+                {
+                    UserName = user.UserName,
+                    Email = user.Email
+                };
+
+                IdentityResult result = await userManager.CreateAsync(appUser, user.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Login");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(user);
+        }
     }
 }
--- CmsShoppingCart/Models/User.cs
+++ CmsShoppingCart/Models/User.cs
@@ -9,13 +9,14 @@ namespace CmsShoppingCart.Models
     public class User
     {
         [Required, MinLength(2, ErrorMessage = "Minimum length is 2")]
+        [Display(Name = "Username")]
         public string UserName { get; set; }
         [Required, EmailAddress]
         public string Email { get; set; }
         [DataType(DataType.Password), Required, MinLength(4, ErrorMessage = "Minimum length is 4")]
         public string Password { get; set; }
 
-        User() { }
+        public User() { }
 
         public User(AppUser appUser)
         {
