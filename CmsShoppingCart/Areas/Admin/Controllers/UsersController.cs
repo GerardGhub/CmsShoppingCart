cda1ffea4e7f4314dfb3e17d34fa2f7ccb9c166e
voja <voja@gmail.com>
2019-11-13 11:02:12 +0100

    84

---
+++ CmsShoppingCart/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<AppUser> userManager;
+
+        public UsersController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
