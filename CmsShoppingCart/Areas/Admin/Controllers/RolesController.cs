6a8d7289f14f4ba8f13a7a40bb42a8a8617664dc
voja <voja@gmail.com>
2019-11-14 08:13:31 +0100

    85

---
+++ CmsShoppingCart/Areas/Admin/Controllers/RolesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsShoppingCart.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RolesController : Controller
+    {
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<AppUser> userManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager, UserManager<AppUser> userManager)
+        {
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
